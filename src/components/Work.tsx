9829767937a780a40add1e62e0b73331dd59362c
Chaitanya Ajay Bhujbal <chaitanyabhujbal912006@gmail.com>
2026-08-16 12:34:23 +0530

    revert: restore original component layout & animation styling

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -151,13 +151,7 @@ const Work = () => {
                         <p className="carousel-description">{project.description}</p>
                         <div className="carousel-tools">
                           <span className="tools-label">Tools & Features</span>
-                          <div className="tools-pills-row">
-                            {project.tools.split(", ").map((tool, tIdx) => (
-                              <span key={tIdx} className="tool-pill">
-                                {tool}
-                              </span>
-                            ))}
-                          </div>
+                          <p>{project.tools}</p>
                         </div>
                         <div className="carousel-links">
                           <a
